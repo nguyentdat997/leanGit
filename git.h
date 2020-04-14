39b4551111d77b8775860653b8c402d2cb308717
Dat <nguyentdat997@gmail.com>
2020-04-14 14:49:38 +0700

    add GiamDan

--- git.h
+++ git.h
@@ -15,4 +15,11 @@ output: array pointer point to array
 ************************************************************/
 int * TangDan(int *array, int n);
 
+/************************************************************
+function GiamDan: sort array in descending order
+input: array pointer point to array
+       n number element of array
+output: array pointer point to array
+************************************************************/
+int * GiamDan(int *array, int n);
 #endif
